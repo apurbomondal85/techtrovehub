442796a6a217fd06ceca9e22adca74a5a08f70b8
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2023-12-06 12:11:08 +0600

    offer product page is complet

--- src/Pages/OfferPage/OfferPage.jsx
+++ src/Pages/OfferPage/OfferPage.jsx
@@ -1,9 +1,40 @@
-
-import React from 'react'
+import { useEffect, useState } from "react";
+import CategorySidebar from "../../Components/CategoryProduct/CategorySidebar"
+import ProductSection from "../../Components/CategoryProduct/ProductSection"
+import SearchBanner from "../../Components/CategoryProduct/SearchBanner"
+import useAxiosSecure from "../../utiliz/useAxiosSecure";
 
 const OfferPage = () => {
+  const axiosSecure = useAxiosSecure();
+  const [handleTabCategory, setHandleTabCategory] = useState("Laptop");
+  const [products, setProducts] = useState();
+  const [searchValue, setSearchValue] = useState();
+
+  useEffect(() => {
+    if (handleTabCategory) {
+      axiosSecure.get(`/offer-category/${handleTabCategory}`).then(res => setProducts(res.data)).catch(err => console.log(err.message));
+    }
+  }, [handleTabCategory])
+
+  useEffect(() => {
+    if (searchValue) {
+      axiosSecure.get("/offer-products")
+        .then(res => {
+          const filtered = res.data.filter((product) => product.productName.toLowerCase().includes(searchValue.toLowerCase()));
+          setProducts(filtered)
+        })
+        .catch(err => console.log(err.message));
+    }
+  }, [searchValue])
+
   return (
-    <div>OfferPage</div>
+    <div>
+      <SearchBanner setSearchValue={setSearchValue} />
+      <div className="mt-12">
+        <CategorySidebar handleTabCategory={setHandleTabCategory} />
+        <ProductSection products={products} />
+      </div>
+    </div>
   )
 }
 
